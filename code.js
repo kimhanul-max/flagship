84ab1b0c99b33b23df01ca619ef1c82fe00c611a
김한울 <gimhan-ul@gimhan-ul-ui-MacBookAir.local>
2022-08-28 17:02:36 +0900

    add-design

---
+++ code.js
@@ -0,0 +1,36 @@
+const icon = document.querySelector("#menu-icon");
+const contents = document.querySelector("nav");
+const app = document.querySelector(".app");
+const flagship = document.querySelector(".flagship");
+const github = document.querySelector(".github");
+const body = document.querySelector(".body");
+const title = document.querySelector(".title");
+function goApp(){
+    location.href = "app.html";
+}
+function goFlagship(){
+    window.open("https://ko.wikipedia.org/wiki/%EA%B9%83%EB%8C%80%EC%A2%85", "_blank");
+}
+function goGithub(){
+    window.open("https://github.com/kimhanul-max/flagship", "_blank");
+}
+function goTitle(){
+    location.href = "index.html"
+}
+
+app.addEventListener("click", goApp);
+flagship.addEventListener("click", goFlagship);
+github.addEventListener("click", goGithub);
+title.addEventListener("click", goTitle);
+
+icon.addEventListener("click", () => {
+    if (contents.dataset.isOpen == "true") {
+        contents.style.display = "none";
+        contents.dataset.isOpen = "false";
+        body.classList.remove("shader");
+    } else {
+        contents.style.display = "block";
+        contents.dataset.isOpen = "true";
+        body.classList.add("shader");
+    }
+})
